43b12662d979a43b614a46792b2b10ae0b1f4b47
sherry <1936839308@qq.com>
2020-05-13 21:50:00 +0800

    icon-position

--- src/button.vue
+++ src/button.vue
@@ -1,31 +1,29 @@
 <template>
-  <button class="g-button">
-    <slot></slot>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="g-icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content"><slot></slot></div>
   </button>
 </template>
 
 <script>
   import Vue from 'vue';
  export default {
-
+props:['icon','iconPosition']
  }
 
 </script>
 
 <style lang="scss" scoped>
   .g-button{
-    font-size:var(--font-size);
-    height:var(--button-height);
-    padding:0 1em;
-    border-radius:var(--border-radius);
-    border:1px solid var(--border-color);
-    background:var(--button-bg);
-    &:hover{
-      border-color:var(--border-color-hover);
-    }
-    &:active{
-      background:var(--button-active-bg);
-    }
+    font-size:var(--font-size);height:var(--button-height);padding:0 1em;
+    border-radius:var(--border-radius);border:1px solid var(--border-color);background:var(--button-bg);
+    display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
+    &:hover{border-color:var(--border-color-hover);}
+    &:active{background:var(--button-active-bg);}
+    &:focus{outline: none;} > .g-icon{order:1;margin-right: .2em;} > .content{order:2}
+    &.icon-right{ > .g-icon{order:2;margin-left: .2em; margin-right: 0;} > .content{order:1;} }
   }
 
 </style>
