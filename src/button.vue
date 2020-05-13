ebce64e5739ae945f0400681f3850485c671ccea
sherry <1936839308@qq.com>
2020-05-13 22:01:17 +0800

    验证 icon-position 是 left || right

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,17 @@
 <script>
   import Vue from 'vue';
  export default {
-props:['icon','iconPosition']
+// props:['icon','iconPosition']
+   props:{
+     icon:{},
+     iconPosition:{
+       type:String,
+       default:'left',
+       validator(value){
+        return value === 'left' || value === 'right'
+       }
+     }
+   }
  }
 
 </script>
