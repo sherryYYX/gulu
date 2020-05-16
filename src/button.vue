85e86819cf035367f88d3a4b75672251dbd069ff
sherry <1936839308@qq.com>
2020-05-16 11:40:44 +0800

    添加loading

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-  <g-icon v-if="icon" :name="icon"></g-icon>
+  <g-icon class="g-icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon name="loading" class="loading"></g-icon>
     <div class="content"><slot></slot></div>
   </button>
 </template>
@@ -27,6 +28,10 @@
 </script>
 
 <style lang="scss" scoped>
+  @keyframes  spin{
+  0%{transform: rotate(0deg);}
+    100%{transform: rotate(360deg);}
+  }
   .g-button{
     font-size:var(--font-size);height:var(--button-height);padding:0 1em;
     border-radius:var(--border-radius);border:1px solid var(--border-color);background:var(--button-bg);
@@ -36,5 +41,7 @@
     &:focus{outline: none;} > .g-icon{order:1;margin-right: .2em;} > .content{order:2}
     &.icon-right{ > .g-icon{order:2;margin-left: .2em; margin-right: 0;} > .content{order:1;} }
   }
-
+.loading{
+  animation: spin 2s linear infinite;
+}
 </style>
