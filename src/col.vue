bd49b465b3a3950dae7fd622bca6ff9dd7b1f9a2
sherry <1936839308@qq.com>
2020-05-19 21:22:07 +0800

    重构row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
-  :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px;">
+  <div class="col" :class="colClass"
+  :style="colStyle">
       <slot></slot>
-    </div>
   </div>
 </template>
 <script>
@@ -19,7 +17,19 @@
     },
     data(){
       return{
-        gutter:0
+        gutter:0,
+      }
+    },
+    computed:{
+      colClass(){
+        let {span,offset}=this
+        return [span && `col-${span}`, offset && `offset-${offset}`]
+      },
+      colStyle(){
+        return{
+          paddingLeft: this.gutter/2+"px",
+          paddingRight: this.gutter/2+'px'
+        }
       }
     }
   }
