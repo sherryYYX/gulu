e1aa79bd52f401546d5c067a14b17b958547cb32
sherry <1936839308@qq.com>
2020-05-27 12:29:46 +0800

    toast

--- src/app.js
+++ src/app.js
@@ -5,13 +5,28 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Col from './col'
 import Row from './row'
+import Header from './header'
+import Layout from './layout'
+import Sider from './sider'
+import  Content from './content'
+import Footer from './footer'
+import Toast from './toast'
+import Plugin from './plugin'
+import plugin from "./plugin"
 
+
+Vue.component('g-header',Header)
+Vue.component('g-layout',Layout)
+Vue.component('g-sider',Sider)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
 Vue.component('g-col',Col)
 Vue.component('g-row',Row)
 Vue.component('g-input',Input)
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.use(plugin)
 
 new Vue({
   el:'#app',
@@ -21,10 +36,32 @@ new Vue({
     loading3: true,
     message: 'hi',
   },
+  created(){
+
+  },
   methods:{
-    inputChange(e){
-      console.log(e)
-    }
+    showToast1(){
+      this.showToast('top')
+    },
+    showToast2(){
+      this.showToast('middle')
+    },
+    showToast3(){
+      this.showToast('bottom')
+    },
+   showToast(position){
+     this.$toast(`要充值智商税${parseInt(Math.random()*100)}`,{
+       position,
+       closeButton:{
+         text:'已充值',
+         callback(){
+           console.log('已充值智商税')
+         },
+       },
+       autoClose:false,
+       autoCloseDelay: 1000
+     })
+   }
   }
 })
 
---
+++ src/toast.vue
@@ -0,0 +1,126 @@
+<template>
+ <div class="toast" ref="toast" :class="toastClasses">
+   <div class="message">
+     <slot></slot>
+   </div>
+   <div class="line" ref="line"></div>
+   <span   class="close"  v-if="closeButton" @click="onClickClose">
+     {{closeButton.text}}
+   </span>
+ </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default {
+     name:'gulu-toast',
+    props:{
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+        }
+      },
+       autoClose:{
+         type:Boolean,
+         default:true
+       },
+      autoCloseDelay:{
+         type:Number,
+          default:8
+      },
+      closeButton:{
+         type:Object,
+          default:()=>{
+          return{
+            text:'关闭', undefined
+          }
+        }
+      }
+    },
+    mounted() {
+      this.updateClose()
+      this.exacAutoClose()
+    },
+    computed: {
+      toastClasses () {
+        return `position-${this.position}`
+      }
+    },
+    methods:{
+      updateClose(){
+        setTimeout(()=>{
+          this.$refs.line.style.height=
+            `${this.$refs.toast.getBoundingClientRect().height}px`
+        },0)
+      },
+       exacAutoClose(){
+         if(this.autoClose===true){
+           setTimeout(()=>{
+             this.close();
+           },this.autoCloseDelay * 1000)
+         }
+       },
+      onClickClose(){
+        this.close()
+        if(this.closeButton && typeof this.closeButton.callback === 'function'){
+          this.closeButton.callback()
+        }
+      },
+      close(){
+        this.$el.remove()
+        this.$emit('close')
+        this.$destroy()
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $line-height: 40px;
+  $toast-min-height: 40px;
+  $toast-bg:rgba(0,0,0,0.75);
+  @keyframes fade-in {
+    0%{opacity: 0}
+    100%{opacity: 1}
+  }
+  .toast{
+    animation: fade-in 1s;
+    min-height: $toast-min-height;
+    line-height: $line-height;
+    position: fixed;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    background: $toast-bg;
+    border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0,0,0,0.75);
+    color:white;
+    padding:0 16px;
+    left: 50%;
+    &.position-top{
+      top:0;
+      transform: translateX(-50%);
+    }
+    &.position-bottom{
+      bottom:0;
+      transform: translateX(-50%);
+    }
+    &.position-middle{
+      top:50%;
+      transform: translate(-50%,-50%);
+    }
+    .message{
+      padding-right:16px;
+    }
+    .line{
+      border-left:1px solid #666;
+    }
+    .close{
+      padding-left: 14px;
+      flex-shrink: 0;
+    }
+  }
+</style>
