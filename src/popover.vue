f9190de4d542cb6be1a013da4106c93df19a6171
sherry <1936839308@qq.com>
2020-06-07 15:13:35 +0800

    解决容器有overflow:hidden 时 出现的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="popover"  @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover"  @click="xxx">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+       <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -17,6 +19,10 @@
         console.log('visable 切换')
         if(this.visible===true){
           setTimeout(()=>{
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {height,width,top,left}=this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top  +window.scrollY + 'px'
             console.log('新增document 监听器')
             let eventHandler = ()=>{
               this.visible=false
@@ -26,6 +32,8 @@
           })
         }
       }
+    },
+    mounted() {
     }
   }
 </script>
@@ -35,12 +43,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    > .content-wrapper{
-      border:1px solid red;
-      position: absolute;
-      bottom: 100%;
-      left:0;
-      box-shadow: 0 0 3px rgba(0,0,0,0.5);
-    }
+  }
+  .content-wrapper{
+    border:1px solid red;
+    position: absolute;
+    transform: translateY(-100%);
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
   }
 </style>
