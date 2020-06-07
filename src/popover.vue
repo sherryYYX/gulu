a1f5276cae660e33a3baa267c71686baacf7df4d
sherry <1936839308@qq.com>
2020-06-07 19:36:32 +0800

    重构为表驱动编程

--- src/popover.vue
+++ src/popover.vue
@@ -24,26 +24,27 @@
     },
     methods:{
       positionContent(){
-        const contentWrapper =this.$refs.contentWrapper
+        const {contentWrapper,triggerWrapper} =this.$refs
         document.body.appendChild(contentWrapper)
-        let {height,width,top,left}=this.$refs.triggerWrapper.getBoundingClientRect()
-        if(this.position === 'top'){
-          contentWrapper.style.left = left + window.scrollX + 'px'
-          contentWrapper.style.top = top  +window.scrollY + 'px'
-        }else if(this.position === 'bottom'){
-            contentWrapper.style.left = left + window.scrollX + 'px'
-            contentWrapper  .style.top = top +height +window.scrollY + 'px'
-        }else if(this.position === 'left'){
-          contentWrapper.style.left = left + window.scrollX + 'px'
-          let height2=contentWrapper.getBoundingClientRect().height
-          contentWrapper.style.top = top+window.scrollY
-            -Math.abs(height-height2)/2+ 'px'
-        }else if(this.position === 'right'){
-          contentWrapper.style.left = left + window.scrollX +width+ 'px'
-          let height2=contentWrapper.getBoundingClientRect().height
-          contentWrapper.style.top = top+window.scrollY
-            -Math.abs(height-height2)/2+ 'px'
+        const {height,width,top,left}= triggerWrapper.getBoundingClientRect()
+        const height2=contentWrapper.getBoundingClientRect().height
+        let positions ={
+          top:{top:top+window.scrollY, left:left + window.scrollX},
+          bottom:{
+            top:top +height +window.scrollY,
+            left:left + window.scrollX
+          },
+          left:{
+            top:top+window.scrollY +(height-height2)/2,
+            left:left + window.scrollX,
+          },
+          right:{
+            top:top+window.scrollY +(height-height2)/2,
+            left:left + window.scrollX +width
+          },
         }
+        contentWrapper.style.left=positions[this.position].left +'px'
+        contentWrapper.style.top=positions[this.position].top+ 'px'
       },
       onClickDocument (event){
         if(this.$refs.contentWrapper.contains(event.target)){return}else{
@@ -105,19 +106,21 @@
       position: absolute;
       left:10px
     }
-    &.position-top{
+    &.position-top {
       transform: translateY(-100%);
       margin-top: -10px;
-      &::before,&::after{
-        left:10px
+      &::before, &::after {
+        left: 10px;
       }
-      &::before{
+      &::before {
         border-top-color: black;
-        top:100%;
+        border-bottom: none;
+        top: 100%;
       }
-      &::after{
+      &::after {
         border-top-color: white;
-        top:calc(100% - 1px);
+        border-bottom: none;
+        top: calc(100% - 1px);
       }
     }
     &.position-bottom{
