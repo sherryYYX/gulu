97864ccae8301d7f798a9c71ff4227e9dddb5fe6
sherry <1936839308@qq.com>
2020-06-17 08:24:06 +0800

    官网基础实现 update

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -11,30 +11,19 @@
     <p>
       <strong>代码</strong>
     </p>
-    <pre style="height: 77%;padding-left: 0;">
-      	//此处class：
-      	//language-xml指的是基于xml语言进行高亮设置
-        //line-numbers指的是使用显示行号
-        <code class="language-xml line-numbers" v-text="html"></code>
-      </pre>
-
 
     <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <script>
-  import Prism from 'prismjs';
-import prismCss from './prism.css'
-  Prism.highlightAll();
   import Button from '../../../src/button'
   export default {
     components:{
       'g-button':Button,
-      'prism':Prism
     },
     data(){
       return{
-        html:`
+        content:`
               <g-button :loading="true">默认按钮</g-button>
               <g-button icon="settings">设置按钮</g-button>
               <g-button :loading="true">加载中</g-button>
